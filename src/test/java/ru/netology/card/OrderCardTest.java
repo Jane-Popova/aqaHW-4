0e31589cd3411f71678cb12e3697ee1db5d89962
Evgenia Popova <jane477@yandex.ru>
2024-10-28 23:16:34 +0300

    added test

---
+++ src/test/java/ru/netology/card/OrderCardTest.java
@@ -0,0 +1,47 @@
+package ru.netology.card;
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+class OrderCardTest {
+
+    public String generateDate(int days, String pattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    void testCorrectInput() {
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        String city = "Лондон";
+        String name = "Иванов Иван";
+        String phone = "+79781234567";
+
+        Selenide.open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue(city);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue(name);
+        $("[data-test-id='phone'] input").setValue(phone);
+        $("[data-test-id='agreement']").click();
+        $(".button__text").click();
+        $("[data-test-id='notification']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(text("Встреча успешно забронирована на " + planningDate));
+    }
+
+}
+
